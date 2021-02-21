0f1bfce1c4d0addc77fa3869eda2831bbbecd44b
jrk8047 <jrk8047@rit.edu>
2021-02-21 09:20:38 -0500

    Added voice sidebar

--- front/src/App.js
+++ front/src/App.js
@@ -7,6 +7,25 @@ import Fibbrick from './components/Fibbrick';
 import Youtube from './components/Youtube';
 import Login from './components/Login';
 
+import { reducer as voxeetReducer } from "@voxeet/react-components"
+import thunkMidleware from "redux-thunk"
+import { combineReducers, createStore, applyMiddleware } from "redux"
+import { ConferenceRoom, VoxeetProvider } from "@voxeet/react-components"
+import "@voxeet/react-components/dist/voxeet-react-components.css"
+
+const settings = {
+  consumerKey: 'QCcMg9I76NPfCgls2CPY4A==',
+  consumerSecret: '68ygdCLzwcGBla7QF9-bs3h2nHmOobUCkE9L58ngMHA=',
+  conferenceAlias: 'RoomName'
+}
+
+const reducers = combineReducers({
+  voxeet: voxeetReducer,
+})
+
+const configureStore = () =>
+  createStore(reducers, applyMiddleware(thunkMidleware))
+
 class App extends React.Component {
 
   constructor(props) {
@@ -49,13 +68,28 @@ class App extends React.Component {
         break;
       default:
         currentComponent = Menu;
-    } 
+    }
+
+    let sidebar = <div></div>;
+    if (this.state.currentActivity != "Login") {
+      sidebar = 
+        <div>  
+          <VoxeetProvider store={configureStore()}>
+            <ConferenceRoom
+              autoJoin
+              consumerKey={settings.consumerKey}
+              consumerSecret={settings.consumerSecret}
+              conferenceAlias={settings.conferenceAlias}
+            />
+          </VoxeetProvider>
+        </div>;
+    }
 
     return (
       <div>
         {currentComponent}
+        {sidebar}
       </div>
-
     );
   }
 }
